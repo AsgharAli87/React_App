df42ff4e635c5ca9808823c61fdb82b1da72beaf
Asghar Ali <asgharnedian40@gmail.com>
2026-03-05 17:25:51 +0500

    commit

--- MedicalCenter/src/components/Footer.jsx
+++ MedicalCenter/src/components/Footer.jsx
@@ -32,7 +32,7 @@ const Footer = () => {
     {/* CopyRight */}
         <div>
             <hr />
-            <p className=' py-5 text-sm text-center '>Copyright © 2025 Prescripto - All Right Reserved.</p>
+            <p className=' py-5 text-sm text-center '>Copyright © 2026 Prescripto Asghar Ali - All Right Reserved .</p>
         </div>
     </div>
   )
